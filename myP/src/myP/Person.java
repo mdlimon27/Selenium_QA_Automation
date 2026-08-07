f5be620c2eeb6f461161d344edbc89185464d643
mimran0 <jmrthan@gmail.com>
2026-08-06 20:07:27 -0700

    Selenium Java

---
+++ myP/src/myP/Person.java
@@ -0,0 +1,3 @@
+package myP;
+
+public record Person(String name, int age) {}
---
+++ myP/src/myP/myA.java
@@ -0,0 +1,12 @@
+package myP;
+
+public class myA {
+public static void main(String[] Args) {
+	//System.out.println("Md Shahajada Imran");
+	
+	Person p=new Person("md Shahajada Imran",40);
+	System.out.println("name: "+p.name());
+	System.out.println("age: "+p.age());
+	
+}
+}
